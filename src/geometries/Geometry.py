c9b442f08c809e924e7e096d1e765bfea2d0e8b7
Stuart Farris <sfarris@stanford.edu>
2022-07-12 06:41:34 -0700

    updated 3d sreamer geometry

--- src/geometries/Geometry.py
+++ src/geometries/Geometry.py
@@ -48,7 +48,7 @@ class Streamer2D(Geometry):
     self.rec_positions = np.array([rec_x.T, rec_z.T]).T
 
 
-class Streamer3D(Geometry):
+class Streamer3D(Geometry.Geometry):
 
   def __init__(self, n_y_src: int, o_y_src: float, d_y_src: float, n_x_src: int,
                o_x_src: float, d_x_src: float, z_src: float, n_y_rec: int,
@@ -56,11 +56,25 @@ class Streamer3D(Geometry):
                d_x_rec: float, z_rec: float):
     src_y = o_y_src + np.arange(n_y_src) * d_y_src
     src_x = o_x_src + np.arange(n_x_src) * d_x_src
-    src_z = np.ones_like(src_x) * z_src
-    self.src_positions = np.array([src_x, src_z]).T
+    src_xx, src_yy = np.meshgrid(src_x, src_y, indexing='ij')
+    src_xx = src_xx.flatten()
+    src_yy = src_yy.flatten()
+    src_zz = np.ones_like(src_xx) * z_src
+    self.src_positions = np.array([src_yy, src_xx, src_zz]).T
 
-    relative_rec_x = o_x_rec + np.arange(n_rec) * d_x_rec
-    rec_x = np.repeat(np.expand_dims(relative_rec_x, axis=0), n_src, axis=0)
-    rec_x = (rec_x.T + src_x).T
-    rec_z = np.ones_like(rec_x) * z_rec
-    self.rec_positions = np.array([rec_x.T, rec_z.T]).T
+    relative_rec_x = o_x_rec + np.arange(n_x_rec) * d_x_rec
+    relative_rec_y = o_y_rec + np.arange(n_y_rec) * d_y_rec
+    relative_rec_yy, relative_rec_xx = np.meshgrid(relative_rec_y,
+                                                   relative_rec_x,
+                                                   indexing='ij')
+    relative_rec_xx = relative_rec_xx.flatten()
+    relative_rec_yy = relative_rec_yy.flatten()
+    relative_rec_zz = np.ones_like(relative_rec_xx) * z_rec
+
+    n_src = len(src_xx)
+    rec_xx = np.repeat(np.expand_dims(relative_rec_xx, axis=0), n_src, axis=0)
+    rec_yy = np.repeat(np.expand_dims(relative_rec_yy, axis=0), n_src, axis=0)
+    rec_xx = (rec_xx.T + src_xx).T
+    rec_yy = (rec_yy.T + src_yy).T
+    rec_zz = np.ones_like(rec_xx) * z_rec
+    self.rec_positions = np.array([rec_yy.T, rec_xx.T, rec_zz.T]).T
