ae2cbe8752e9ef95eb369db1a51f5b152b26c19c
Stuart Farris <sfarris@stanford.edu>
2023-01-11 17:56:40 -0800

    fwi MVP

--- pyseis/fwi_test.py
+++ pyseis/fwi_test.py
@@ -5,38 +5,109 @@ from pyseis.wave_equations import acoustic_isotropic
 from pyseis.wavelets.acoustic import Acoustic2D
 from pyseis import inversion
 
-# @pytest.mark.gpu
-# def test_init_fwi():
-#   # Arrange
-#   acoustic_2d = acoustic_isotropic.AcousticIsotropic2D(
-#       model=vp_model_half_space,
-#       model_sampling=(D_X, D_Z),
-#       model_padding=(N_X_PAD, N_Z_PAD),
-#       wavelet=ricker_wavelet,
-#       d_t=D_T,
-#       src_locations=src_locations,
-#       rec_locations=fixed_rec_locations,
-#       gpus=I_GPUS)
-#   # init
-#   fwi_prob = inversion.Fwi(wave_eq_solver=acoustic_2d
-#       data=data,
-#       num_iter=3)
+N_X = 100
+D_X = 10.0
+N_Z = 50
+D_Z = 5.0
+model_sampling = (D_X, D_Z)
+N_X_PAD = 25
+N_Z_PAD = 20
+VP_1 = 1500
+VP_2 = 2500
 
-# @pytest.mark.gpu
-# def test_run_fwi():
-#   # Arrange
-#   acoustic_2d = acoustic_isotropic.AcousticIsotropic2D(
-#       model=vp_model_half_space,
-#       model_sampling=(D_X, D_Z),
-#       model_padding=(N_X_PAD, N_Z_PAD),
-#       wavelet=ricker_wavelet,
-#       d_t=D_T,
-#       src_locations=src_locations,
-#       rec_locations=fixed_rec_locations,
-#       gpus=I_GPUS)
-#   # init
-#   fwi_prob = inversion.Fwi(wave_eq_solver=acoustic_2d
-#       data=data,
-#       num_iter=3)
-#   # run
-#   inv_model, hist = fwi_prob.run()
+N_T = 1000
+D_T = 0.01
+
+DELAY = 2.0
+DOM_FREQ = 5.0
+CONFIG_RICKER = {'delay': DELAY, 'dom_freq': DOM_FREQ}
+
+N_SRCS = 4
+Z_SHOT = 10.0
+N_REC = 10
+D_X_REC = 11.0
+Z_REC = 5.0
+#expected values
+SUB = 3
+I_GPUS = [0, 1, 2, 3]
+
+
+@pytest.fixture
+def ricker_wavelet():
+  ricker = Acoustic2D.AcousticIsotropicRicker2D(N_T, D_T, DOM_FREQ, DELAY)
+  return ricker.get_arr()
+
+
+@pytest.fixture
+def src_locations():
+  x_locs = np.linspace(0.0, N_X * D_X, num=N_SRCS)
+  z_locs = np.ones(N_SRCS) * Z_SHOT
+  return np.array([x_locs, z_locs]).T
+
+
+@pytest.fixture
+def rec_locations():
+  rec_x_locs = np.arange(N_REC) * D_X_REC
+  rec_z_locs = np.ones(N_REC) * Z_REC
+  return np.array([rec_x_locs, rec_z_locs]).T
+
+
+@pytest.fixture
+def vp_model_half_space():
+  vp_model = np.zeros((N_X, N_Z))
+  vp_model[:, :N_Z // 2] = VP_1
+  vp_model[:, N_Z // 2:] = VP_2
+  return vp_model
+
+
+@pytest.fixture
+def vp_model_full_space():
+  return VP_1 * np.ones((N_X, N_Z))
+
+
+@pytest.fixture
+def acoustic_2d_weq_solver(ricker_wavelet, src_locations, rec_locations,
+                           vp_model_half_space):
+  return acoustic_isotropic.AcousticIsotropic2D(model=vp_model_half_space,
+                                                model_sampling=(D_X, D_Z),
+                                                model_padding=(N_X_PAD,
+                                                               N_Z_PAD),
+                                                wavelet=ricker_wavelet,
+                                                d_t=D_T,
+                                                src_locations=src_locations,
+                                                rec_locations=rec_locations,
+                                                gpus=I_GPUS)
+
+
+@pytest.mark.gpu
+def test_init_fwi(acoustic_2d_weq_solver, vp_model_full_space):
+  # Arrange
+  obs_data = np.zeros_like(acoustic_2d_weq_solver.data_sep.getNdArray())
+
+  # Run
+  fwi_prob = inversion.Fwi(wave_eq_solver=acoustic_2d_weq_solver,
+                           obs_data=obs_data,
+                           starting_model=vp_model_full_space,
+                           num_iter=3)
+
+
+@pytest.mark.gpu
+def test_init_fwi(acoustic_2d_weq_solver, vp_model_half_space,
+                  vp_model_full_space):
+  # Arrange
+  obs_data = acoustic_2d_weq_solver.forward(vp_model_half_space)
+  fwi_prob = inversion.Fwi(wave_eq_solver=acoustic_2d_weq_solver,
+                           obs_data=obs_data,
+                           starting_model=vp_model_full_space,
+                           num_iter=3,
+                           solver_type='nlcg',
+                           model_bounds=[1300, 4500],
+                           iterations_per_save=1)
+
+  # Run
+  history = fwi_prob.run()
+
+  # Assert
+  all_keys = ['inv_mod', 'gradient', 'model', 'residual', 'obj']
+  for key in all_keys:
+    assert history[key] is not None
--- pyseis/inversion.py
+++ pyseis/inversion.py
@@ -1,33 +1,196 @@
+from datetime import datetime
+import pathlib
+import os
+import sys_util
+
+import pyProblem as Prblm
+import pyStepper as Stepper
+import pyStopper as Stopper
+import pyOperator as Operator
+import pyNonLinearSolver
+import genericIO
+
+SOLVERS = {
+    'nlcg': pyNonLinearSolver.NLCGsolver,
+    'lbfgs': pyNonLinearSolver.LBFGSsolver,
+    'lbfgsb': pyNonLinearSolver.LBFGSBsolver
+}
+STEPPERS = {
+    'parabolic': Stepper.ParabolicStep,
+    'linear': Stepper.ParabolicStep,
+    'parabolicNew': Stepper.ParabolicStepConst
+}
+
+
 class Fwi():
 
-  def __init__(wave_eq_solver, data, num_iter, model_bounds=None):
-    # create the fwi operator which includes the nonlinear and linearized wave eq
-    self.fwi_op = self._make_fwi_op(wave_eq_solver)
+  def __init__(self,
+               wave_eq_solver,
+               obs_data,
+               starting_model,
+               num_iter,
+               model_bounds=None,
+               solver_type='lbfgs',
+               stepper_type='parabolic',
+               max_steps=None,
+               work_dir='wrk',
+               prefix=None,
+               iterations_per_save=10,
+               iterations_in_buffer=3):
+    # check that the observed data matches the data space of the wave_eq_solver
+    if wave_eq_solver._get_data().shape != obs_data.shape:
+      raise RuntimeError(
+          'the data space of the provided wave_eq_solver must match the shape of the provided obs_data'
+      )
 
-    # create inverse problem
-    self.wave_eq_solver.sep_data.getNdArray()[:] = data
-    self.problem = self._make_problem(self.survey.wave_equation.sep_model,
-                                      self.survey.wave_equation.sep_data,
+    # check that the initial model the model space of the wave_eq_solver
+    if wave_eq_solver._get_model().shape != starting_model.shape:
+      raise RuntimeError(
+          'the model space of the provided wave_eq_solver must match the shape of the provided starting_model'
+      )
+
+    self.wave_eq_solver = wave_eq_solver
+
+    # create the fwi operator which includes the nonlinear and linearized wave eq
+    self.fwi_op = self._make_fwi_op(self.wave_eq_solver)
+
+    # create inversion problem
+    self.wave_eq_solver._set_data(obs_data)
+    self.wave_eq_solver._setup_model(starting_model)
+    self.problem = self._make_problem(self.wave_eq_solver,
                                       self.fwi_op,
                                       model_bounds=model_bounds)
 
     # create solver
-    self.solver = self._make_solver(config['inversion'])
+    self.solver = self._make_solver(num_iter,
+                                    solver_type=solver_type,
+                                    stepper_type=stepper_type,
+                                    max_steps=max_steps,
+                                    work_dir=work_dir,
+                                    prefix=prefix,
+                                    iterations_per_save=iterations_per_save,
+                                    iterations_in_buffer=iterations_in_buffer)
+
+  def run(self, verbose=1):
+    self.solver.run(self.problem, verbose=verbose)
+    self.history = load_history(self.work_dir, self.prefix,
+                                self.wave_eq_solver._truncate_model)
+
+    return self.history
 
   def _make_fwi_op(self, wave_eq_solver):
-    nonlinear_op = wave_eq_solver.wave_prop_cpp_op
-    linearized_op = wave_eq_solver.linearized_wave_prop_cpp_op
+    if wave_eq_solver._jac_wave_op is None:
+      wave_eq_solver._setup_jac_wave_op()
 
-    return Operator.NonLinearOperator(nonlinear_op, linearized_op,
-                                      linearized_op.set_background)
+    return Operator.NonLinearOperator(
+        wave_eq_solver._nl_wave_op, wave_eq_solver._jac_wave_op,
+        wave_eq_solver._jac_wave_op.set_background)
 
-  def _make_problem(self, sep_model, sep_data, fwi_op, model_bounds=None):
-    if model_bounds is not None:
-      min_bound = model_bounds[0]
-      max_bound = model_bounds[1]
+  def _make_problem(self, wave_eq_solver, operator, model_bounds=None):
+    if model_bounds is None:
+      min_bound = None
+      max_bound = None
+    else:
+      min_bound = self._make_bounds(wave_eq_solver, model_bounds[0])
+      max_bound = self._make_bounds(wave_eq_solver, model_bounds[1])
 
-    return Prblm.ProblemL2NonLinear(sep_model,
-                                    sep_data,
-                                    fwi_op,
+    return Prblm.ProblemL2NonLinear(wave_eq_solver.model_sep,
+                                    wave_eq_solver.data_sep,
+                                    operator,
                                     minBound=min_bound,
                                     maxBound=max_bound)
+
+  def _make_bounds(self, wave_eq_solver, bound):
+    bound_sep = wave_eq_solver.model_sep.clone()
+    bound_arr = bound_sep.getNdArray()
+    bound_arr[bound_arr != 0.0] == bound
+
+    return bound_sep
+
+  def _make_solver(self,
+                   num_iter,
+                   solver_type='lbfgs',
+                   stepper_type='parabolic',
+                   max_steps=None,
+                   work_dir='./.wrk',
+                   prefix=None,
+                   iterations_per_save=10,
+                   iterations_in_buffer=3,
+                   overwrite=True):
+
+    if solver_type not in SOLVERS.keys():
+      raise RuntimeError(
+          f'{solver_type} not a valid solver type. Options are: { SOLVERS.keys()}'
+      )
+    # set working directory
+    self.work_dir = pathlib.Path(work_dir)
+    self.work_dir.mkdir(exist_ok=overwrite)
+
+    # set prefix
+    self.prefix = prefix
+    if prefix is None:
+      self.prefix = datetime.now().strftime('%Y-%m-%dT%H%M%SZ')
+
+    # make logger
+    self.logger_filename = self.work_dir / (self.prefix + '.log')
+    self.logger = sys_util.logger(str(self.logger_filename))
+
+    # make stopper
+    self.stopper = Stopper.BasicStopper(niter=num_iter)
+
+    # make solver
+    solver = SOLVERS[solver_type](self.stopper, self.logger)
+
+    # set solver parameters
+    if max_steps is not None:
+      solver.max_steps = max_steps
+    if stepper_type not in STEPPERS.keys():
+      raise RuntimeError(
+          f'{stepper_type} not a valid stepper type. Options are: { STEPPERS.keys()}'
+      )
+    solver.stepper = STEPPERS[stepper_type]()
+    if stepper_type == 'linear':
+      solver.stepper.eval_parab = False
+
+    # set history defaults
+    solver.setDefaults(save_obj=1,
+                       save_res=1,
+                       save_grad=1,
+                       save_model=1,
+                       prefix=str(self.work_dir / self.prefix),
+                       iter_buffer_size=iterations_in_buffer,
+                       iter_sampling=iterations_per_save,
+                       flush_memory=1)
+
+    return solver
+
+
+def load_history(work_dir, prefix, truncate_padding_func=None):
+  model_space_keys = ['inv_mod', 'gradient', 'model']
+  other_keys = ['residual', 'obj']
+
+  history = {}
+
+  # for model space history files, truncate padding by default
+  for key in model_space_keys:
+    arr = None
+    filename = work_dir / (prefix + f'_{key}.H')
+    if filename.is_file():
+      arr = _load_sep_to_np(filename)
+      if truncate_padding_func:
+        arr = truncate_padding_func(arr)
+    history[key] = arr
+
+  # laod other history files
+  for key in other_keys:
+    arr = None
+    filename = work_dir / (prefix + f'_{key}.H')
+    if filename.is_file():
+      arr = _load_sep_to_np(filename)
+    history[key] = arr
+
+  return history
+
+
+def _load_sep_to_np(sep_fn):
+  return genericIO.defaultIO.getVector(str(sep_fn)).getNdArray()
